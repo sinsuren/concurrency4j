f2629f635ce83da70a6f74c30a30446f5cbeaf4c
Surendra Singh <surendra.singh@cred.club>
2024-09-27 11:07:24 +0530

    FooBar: with Semaphor Lock

--- src/main/java/in/sinsuren/concurrency/foo/bar/FooBar.java
+++ src/main/java/in/sinsuren/concurrency/foo/bar/FooBar.java
@@ -1,54 +1,30 @@
 package in.sinsuren.concurrency.foo.bar;
 
-import java.security.cert.CertPath;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.Semaphore;
 
 public class FooBar {
 
-  private final Lock lock = new ReentrantLock();
-
-  private volatile boolean fooTurn = true;
-  private final Condition fooTurnCondition = lock.newCondition();
-  private final Condition barTurnCondition = lock.newCondition();
-
   private int n;
+  private Semaphore fooSemaphore = new Semaphore(1);
+  private Semaphore barSemaphore = new Semaphore(0);
 
   public FooBar(int n) {
     this.n = n;
   }
 
   public void foo(Runnable printFoo) throws InterruptedException {
-
     for (int i = 0; i < n; i++) {
-
-      lock.lock();
-
-      while (!fooTurn) {
-        fooTurnCondition.await();
-      }
-      // printFoo.run() outputs "foo". Do not change or remove this line.
+      fooSemaphore.acquire();
       printFoo.run();
-      fooTurn = false;
-      barTurnCondition.signal();
+      barSemaphore.release();
     }
   }
 
   public void bar(Runnable printBar) throws InterruptedException {
-
     for (int i = 0; i < n; i++) {
-
-      lock.lock();
-
-      while (fooTurn) {
-        barTurnCondition.await();
-      }
-      // printBar.run() outputs "bar". Do not change or remove this line.
-
+      barSemaphore.acquire();
       printBar.run();
-      fooTurn = true;
-      fooTurnCondition.signal();
+      fooSemaphore.release();
     }
   }
 }
