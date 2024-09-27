2d44601df30e46c3f475426032a051a8057c9db8
Surendra Singh <surendra.singh@cred.club>
2024-09-27 10:19:08 +0530

    FooBar: With boolean flag synchronization

--- src/main/java/in/sinsuren/concurrency/foo/bar/FooBar.java
+++ src/main/java/in/sinsuren/concurrency/foo/bar/FooBar.java
@@ -1,30 +1,40 @@
 package in.sinsuren.concurrency.foo.bar;
 
-import java.util.concurrent.Semaphore;
-
 public class FooBar {
 
+  private boolean fooTurn = true;
   private int n;
 
   public FooBar(int n) {
     this.n = n;
   }
 
-  public void foo(Runnable printFoo) throws InterruptedException {
+  public synchronized void foo(Runnable printFoo) throws InterruptedException {
 
     for (int i = 0; i < n; i++) {
 
+      while (!fooTurn) {
+        wait();
+      }
       // printFoo.run() outputs "foo". Do not change or remove this line.
       printFoo.run();
+      fooTurn = false;
+      notifyAll();
     }
   }
 
-  public void bar(Runnable printBar) throws InterruptedException {
+  public synchronized void bar(Runnable printBar) throws InterruptedException {
 
     for (int i = 0; i < n; i++) {
 
+      while (fooTurn) {
+        wait();
+      }
       // printBar.run() outputs "bar". Do not change or remove this line.
+
       printBar.run();
+      fooTurn = true;
+      notifyAll();
     }
   }
 }
